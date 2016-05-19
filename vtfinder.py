5dc6d3e7c502485edf9cb1bf4d5adf58e23e63ec
m0t <you@example.com>
2016-05-19 09:50:23 +0100

    changes

--- vtfinder.py
+++ vtfinder.py
@@ -39,6 +39,7 @@ def breakhandler64(bp):
     return False
 
 def breakhandler32(bp):
+    print("HeapAlloc break")
     chunkUserAddress = ptrDWord(reg('esp')+0x0C)
     heap = ptrDWord(reg('esp')+0x04)
     if chunkUserAddress!=0 and (heapRequested==0 or heap==heapRequested):
@@ -66,6 +67,12 @@ def isAddressWithinLoadedModules(address):
                 ret = True
     return ret
 
+def isX86():
+    if dbgCommand(".effmach").find("x86 compatible (x86)") > 0:
+        return True
+    else: 
+        return False
+
 def main():
     global heapRequested
     # Fix symbols
@@ -76,13 +83,18 @@ def main():
     # Setup handler for delay loaded modules
     myEventHandler= fvtEventHandler()
     # Get arch and heap params
-    arch32 = (sys.argv[1].lower()=='X86'.lower()) if len(sys.argv)>1 else False
-    heapRequested = int(sys.argv[2], 16) if len(sys.argv)>2 else 0
+    #arch32 = (sys.argv[1].lower()=='X86'.lower()) if len(sys.argv)>1 else False
+    arch32 = isX86()
+    heapRequested = int(sys.argv[1], 16) if len(sys.argv)>1 else 0
     # Run x86 or x64 version
-    setBp(module('ntdll').offset('RtlFreeHeap'), breakhandler32 if arch32 else breakhandler64)
+    if arch32:
+        setBp(module('ntdll').offset('RtlFreeHeap'), breakhandler32)
+    else:
+        setBp(module('ntdll').offset('RtlFreeHeap'), breakhandler64)
     dprintln("Arch is %s and heap is %#x %s" % ("X86" if arch32 else "X64", heapRequested, "(any)" if heapRequested == 0 else ""))
-    dprintln("Currently monitored modules:")
-    printModules()
+    dprintln("Monitoring started")
+    #dprintln("Currently monitored modules:")
+    #printModules()
     go()
 
 if __name__ == '__main__' :
